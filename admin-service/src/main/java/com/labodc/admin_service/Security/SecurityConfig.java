8e7d63a05494dfd56df31cd466a15e525960d9ef
TranQuangTruc <tranquangtruc19@gmail.com>
2025-12-28 20:12:21 +0700

    THIẾT LẬP NỀN TẢNG CHO LAB ADMIN SERVICE

---
+++ admin-service/src/main/java/com/labodc/admin_service/Security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.labodc.admin_service.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(
+                    "/admin/health",
+                    "/actuator/**"
+                ).permitAll()
+                .anyRequest().denyAll()
+            )
+            .httpBasic(Customizer.withDefaults()); // tạm thời
+
+        return http.build();
+    }
+}
